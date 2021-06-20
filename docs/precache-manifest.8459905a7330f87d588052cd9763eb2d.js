2e91590dd595c2c0a9dfb1625eb847af1e8f23bd
Angelk90 <angelo.k90@hotmail.it>
2021-06-20 18:01:47 +0200

    Fix homepage

---
+++ docs/precache-manifest.8459905a7330f87d588052cd9763eb2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd2f339bcfafcd77763a",
+    "url": "/webviewer-react-sample/static/css/main.94ae8936.chunk.css"
+  },
+  {
+    "revision": "bd2f339bcfafcd77763a",
+    "url": "/webviewer-react-sample/static/js/main.bd2f339b.chunk.js"
+  },
+  {
+    "revision": "78d43f1a99c80cd94a28",
+    "url": "/webviewer-react-sample/static/js/1.78d43f1a.chunk.js"
+  },
+  {
+    "revision": "23dd03fbedbe42b205ef",
+    "url": "/webviewer-react-sample/static/js/runtime~main.23dd03fb.js"
+  },
+  {
+    "revision": "9b0e30494d64cdb0cca746c258021cf4",
+    "url": "/webviewer-react-sample/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c5b1cf1e67a651ef98b854f7a28aa1f.js"
+  "/webviewer-react-sample/precache-manifest.8459905a7330f87d588052cd9763eb2d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/webviewer-react-sample/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
